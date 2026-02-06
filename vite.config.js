1c4ea83394f45756cd2928a767b02adb8f5c78c0
OmarDST <omar.ibrahim@dst.com.bn>
2026-02-06 09:59:22 +0800

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/tic-tac-toe-logic-check/",
   server: {
     allowedHosts: ["lordsayur-2.local"],
   },
